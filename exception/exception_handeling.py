83ea8a415a3cb2065807dcdd203c63a2336fb112
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 03:13:05 +0530

    template of all the exceptions

---
+++ exception/exception_handeling.py
@@ -0,0 +1,31 @@
+# Handeling all the exception with certain messages
+
+class DuplicateEnrollmentException:
+    pass
+
+class CourseNotFoundException:
+    pass
+
+class StudentNotFoundException:
+    pass
+
+class TeacherNotFoundException:
+    pass
+
+class PaymentValidationException:
+    pass
+
+class InvalidStudentDataException:
+    pass
+
+class InvalidCourseDataException:
+    pass
+
+class InvalidEnrollmentDataException:
+    pass
+
+class InvalidEnrollmentDataException:
+    pass
+
+class InsufficientFundsException:
+    pass
