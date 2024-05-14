b6b8b17480bf832d2df0ff018f0672b751c17132
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 02:16:48 +0530

    template of enrollment_services

---
+++ dao/enrollment_services.py
@@ -0,0 +1,9 @@
+# Creating all the enrollment related services(CRUD) at same place (encapsulation)
+
+class Enrollment_services:
+
+    def getStudent(self,enrollment):
+        pass
+
+    def getCourse(self,enrollment):
+        pass
