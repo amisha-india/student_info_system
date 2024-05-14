088525dceb673149908ce2dd8a52359e95baa195
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 02:10:15 +0530

    template for course_services

---
+++ dao/course_services.py
@@ -0,0 +1,21 @@
+# Creating all the course related services(CRUD) at same place (encapsulation)
+
+class course_services:
+
+    def AssignTeacher(self,course,teacher_id):
+        pass
+    
+    def UpdateCourseInfo(self, Course):
+        pass
+
+    def DisplayCourseInfo(self):
+        pass
+
+    def GetEnrollments(self, courseID):
+       pass
+
+    def GetTeacher(self, courseID):
+       pass 
+
+    def CreateCourse(self, Course):
+        pass
