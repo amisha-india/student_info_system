7104aefd95cb464d6f8381314acc066bf2686bbc
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 01:09:57 +0530

    Barreling for extensability

---
+++ entity/__init__.py
@@ -0,0 +1,7 @@
+# Putting all in barrel for more extensability and maintance
+
+from .student import Student
+from .course import Course
+from .enrollment import Enrollment
+from .teacher import Teacher
+from .payment import Payment
