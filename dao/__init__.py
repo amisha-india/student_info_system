f6ad4c0ae9598cd1496c041bcc9d5d8e2ed931c2
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 02:49:58 +0530

    Barreling for extensibility

---
+++ dao/__init__.py
@@ -0,0 +1,8 @@
+# Barreling
+
+from .course_services import Course_services
+from .enrollment_services import Enrollment_services
+from .payment_services import Payment_services
+from .SIS import SIS
+from .student_services import Student_services
+from .teacher_services import Teacher_services
---
+++ dao/payment_services.py
@@ -0,0 +1,11 @@
+#Creating all the payment related services(CRUD) at same place (encapsulation)
+class Payment_services:
+    
+    def GetStudent(self, payment_id):
+        pass
+
+    def GetPaymentAmount(self,payment_id):
+        pass
+
+    def GetPaymentDate(self,payment_id):
+        pass
