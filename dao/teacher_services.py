6f642c14d2dfedf43099a09f4e30b01785ba5ae7
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 02:32:49 +0530

    teacher services template

---
+++ dao/teacher_services.py
@@ -0,0 +1,12 @@
+# Creating all the teacher related services(CRUD) at same place (encapsulation)
+
+class Teacher_services:
+
+    def UpdateTeacherInfo(self, teacher_id):
+        pass
+
+    def DisplayTeacherInfo(self, teacher_id):
+        pass
+
+    def GetAssignedCourses(self, teacher_id):
+        pass
