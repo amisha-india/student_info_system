e98c4a5c52477de2f76eb4148b5bb84759d71a16
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 02:02:52 +0530

    created template for student services

---
+++ dao/student_services.py
@@ -0,0 +1,27 @@
+# Creating all the student related services(CRUD) at same place (encapsulation)
+
+class student_services:
+
+    def EnrollInCourse(self, enrollments):
+        pass
+
+    def UpdateStudentInfo(self, Student):
+        pass
+
+    def makePayment(self, Payment):
+        pass
+
+    def displayStudentInfo(self, studentID):
+        pass
+
+    def getEnrolledCourses(self, studentID):
+        pass
+
+    def getPaymentHistory(self, studentID):
+        pass
+
+    def createStudent(self, Student):
+        pass
+
+    def getStudent(self, studID):
+        pass
