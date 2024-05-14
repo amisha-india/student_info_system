9fd652875c80c863ce692975ddf939b8c8e50ea4
amisha.india <kumariamisha2022@gmail.com>
2024-05-14 23:41:40 +0530

    creating teacher class with required attributes

---
+++ entity/teacher.py
@@ -0,0 +1,10 @@
+# Teacher class with its attributes
+
+class Teacher:
+
+    def __init__(self, teacher_id, first_name, last_name, email):
+
+        self.teacher_id = teacher_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
