12bcf4292cb60be3d1196a3812f67cdb1773a7de
amisha.india <kumariamisha2022@gmail.com>
2024-05-16 01:33:48 +0530

    model with getter and setters

--- entity/course.py
+++ entity/course.py
@@ -2,10 +2,25 @@
 
 class Course:
 
-    def __init__(self, course_id, course_name, course_code, instructor_name):
+    def __init__(self, course_id, course_name, credit, teacher_id):
 
         self.course_id = course_id
         self.course_name = course_name
-        self.course_code = course_code
-        self.instructor_name = instructor_name
-        self.enrollments = None
+        self.credit = credit
+        self.teacher_id = teacher_id
+
+        self.enrollments = []
+
+    #Getters
+    def get_course_id(self):
+        return self.__course_id
+
+    def get_course_name(self):
+        return self.__course_name
+
+    #Setters
+    def set_course_id(self, course_id):
+        self.__course_id = course_id
+
+    def set_course_name(self, course_name):
+        self.__course_name = course_name
