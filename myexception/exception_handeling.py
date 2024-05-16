f514db9b2e84c0905ab6924cb3da52fa133a75d5
amisha.india <kumariamisha2022@gmail.com>
2024-05-16 21:04:47 +0530

    added for handling more exception

--- myexception/exception_handeling.py
+++ myexception/exception_handeling.py
@@ -55,3 +55,8 @@ class InvalidTeacherDataException(Exception):
     def __init__(self,message):
         self.message = message
         super().__init__(self.message)
+
+class InvalidDataException(Exception):
+    def __init__(self,message):
+        self.message = message
+        super().__init__(self.message)
