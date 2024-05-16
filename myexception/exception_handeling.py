d9adb732578dbdc8ba621c376a50d86958c4dd07
amisha.india <kumariamisha2022@gmail.com>
2024-05-16 18:08:12 +0530

    Added few more exception according to requirement

--- myexception/exception_handeling.py
+++ myexception/exception_handeling.py
@@ -50,3 +50,8 @@ class InsufficientFundsException(Exception):
     def __init__(self,message):
         self.message = message
         super().__init__(self.message)
+
+class InvalidTeacherDataException(Exception):
+    def __init__(self,message):
+        self.message = message
+        super().__init__(self.message)
