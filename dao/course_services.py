11f0374355785c638a65aeb09bab39c24ed8fa68
amisha.india <kumariamisha2022@gmail.com>
2024-05-17 16:03:31 +0530

    modified to resolve certain issues

--- dao/course_services.py
+++ dao/course_services.py
@@ -1,3 +1,4 @@
+
 from util import *
 from myexception.exception_handeling import *
 from entity.course import Course
@@ -8,8 +9,10 @@ class Course_management():
         self.conn = DBConnUtil.getConnection()
 
     #Assign teacher to course based on course_id
-    def assign_teacher(self,course_id,teahcer_id):
+    def assign_teacher(self):
         try:
+            course_id = int(input("Enter the course id:"))
+            teahcer_id = int(input("Enter the teacher id:"))
             self.cursor.execute("UPDATE Courses SET teacher_id=? WHERE course_id = ?"
                                 , (teahcer_id, course_id))
             self.connection.commit()
@@ -18,15 +21,16 @@ class Course_management():
         raise StudentNotFoundException(f"Error the student not found with given :{course_id}")
     
     #update course info based on course_id
-    def update_course_info(self,course_id):
+    def update_course_info(self):
         try:
             course_id = int(input("Enter course_id :"))
             course_name = input("Enter course name :")
-            teacher_id = int(input("Enter teacher_id :"))
+            course_code = input("Enter the course code:")
             credits = int(input("Enter credits :"))
+            teacher_id = int(input("Enter teacher_id :"))
             stmt = self.conn.cursor()
-            stmt.execute("UPDATE courses SET course_name=?, teacher_id=?, credits=? WHERE course_id=?",
-                     (course_name,teacher_id,credits,course_id))
+            stmt.execute("UPDATE courses SET course_name=?,course_code=?, teacher_id=?, credits=? WHERE course_id=?",
+                     (course_name,course_code,teacher_id,credits,course_id))
             self.conn.commit()
             print("course updated successfully!")
         except Exception as e:
@@ -34,7 +38,7 @@ class Course_management():
             raise  CourseNotFoundException(f"No such course found with ID: {course_id}")
 
     #Dispaying the course information based on course_id
-    def display_course_info(self,course_id):
+    def display_course_info(self):
         try:
             course_id = int(input("Enter course_id: "))
             print("Finding for course with ID:", course_id)
@@ -45,9 +49,10 @@ class Course_management():
 
             row = stmt.fetchone()
             if row:
-                course_id, course_name, credits,teacher_id =row
+                course_id, course_name,course_code, credits,teacher_id =row
                 print(f"Course ID: {course_id}")
                 print(f"Course Name: {course_name}")
+                print(f"Course Code: {course_code}")
                 print(f"credits: {credits}")
                 print(f"teacher_id: {teacher_id}")
                 print("course details retrieved successfully!")
@@ -62,7 +67,7 @@ class Course_management():
             return None
 
     #Getting the enrollments based on course_id
-    def get_enrollments(self,course_id):
+    def get_enrollments(self):
         try:
             course_id = int(input("Enter course_id: "))
             print("Finding for enrollments with ID:", course_id)
@@ -91,7 +96,7 @@ class Course_management():
             return None
 
     #Getting Teacher info based on course id
-    def get_teacher(self,course_id):
+    def get_teacher(self):
         try:
             course_id = int(input("Enter course id :"))
             self.cursor.execute("select first_name,last_name,course_name from teacher inner join courses on courses.teacher_id=teacher.teacher_id where course_id=?"
